42acf4f116ec78b6fec4766e51ac7b1167dec0a7
kategeeper <110825114+kategeeper@users.noreply.github.com>
2022-08-20 00:16:58 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 // https://en.wikipedia.org/wiki/Maurer_rose#p5.js_Code
 
-setup=_=>{createCanvas(W=600,W);background(0);d=random(2,9);n=random(3,9)}
+setup=_=>{createCanvas(W=windowWidth,H=windowHeight);background(0);d=random(2,9);n=random(3,9)}
 draw=_=>{background(0,50);noFill()
 stroke(``)
 frameRate(12)
 w=W/2
+h=H/2
 beginShape()
-translate(w,w)
+translate(w,h)
 for (t=360;t--;){
 n+=.000000013;d+=.00000001
 k=t*d
