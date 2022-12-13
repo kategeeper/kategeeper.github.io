1fa8ec29db2d504a1c837dec253edd211dcc68f0
kategeeper <110825114+kategeeper@users.noreply.github.com>
2022-12-14 09:27:30 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
 // https://en.wikipedia.org/wiki/Maurer_rose#p5.js_Code
-setup=_=>{let cnv=createCanvas(W=100,W);background(255);d=random(3,54);n=random(2,13);cnv.position(fixed);}
-draw=_=>{background(0,255);noFill()
-stroke(55,55,255)
-frameRate(12)
-w=W/2
-beginShape()
-translate(w,w)
-for (t=360;t--;){
-n+=.000000001;d+=.000000001
-k=t*d
-r=(w)*sin(n*k)
-x=r*cos(k)
-y=r*sin(k)
-vertex(x,y)}endShape()}
+function sketch_Flwr(p) {
+  p.setup = function () {
+    p.createCanvas(W=200,W);
+    p.frameRate(12);
+    
+    p.d = p.random(3,54);
+    p.n = p.random(2,13);
+  }
+
+  p.draw = function () {
+    p.background(0);
+    p.noFill()
+    p.stroke(55,55,255)
+    w=W/2
+    p.beginShape()
+    p.translate(w,w)
+    for (p.t=360;p.t--;) {
+      p.n+=0.000000001;
+      p.d+=0.000000001;
+      p.k=p.t*p.d;
+      p.r=(w)*p.sin(p.n*p.k);
+      p.x=p.r*p.cos(p.k);
+      p.y=p.r*p.sin(p.k);
+      p.vertex(p.x,p.y);
+    }
+    p.endShape();
+  }
+}
+new p5(sketch_Flwr, 'Flwr')
