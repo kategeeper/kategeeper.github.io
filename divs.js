de1482f5ed9f6e8600afb266c2e311e308079ef1
kategeeper <110825114+kategeeper@users.noreply.github.com>
2022-12-21 21:33:19 +1100

    Update divs.js

--- divs.js
+++ divs.js
@@ -1,32 +1,7 @@
 
-  //function setup() {
-    //noCanvas();
-
-    // let Flwr = createDiv();
-    // Flwr.class('card');
-    // Flwr.id('Flwr');
-
-    // let img01 = createDiv();
-    // img01.class('card');
-    // img01.style('background-image','url("https://live.staticflickr.com/65535/52354298631_b7fb91e32f_c.jpg")');
-
-    // let img02 = createDiv();
-    // img02.class('card');
-    // img02.style('background-image','url("https://live.staticflickr.com/65535/52479394858_694416ace9_b.jpg")');
-
-    // let img03 = createDiv();
-    // img03.class('card');
-    // img03.style('background-image','url("https://live.staticflickr.com/65535/52479126474_b2c236fe0b_b.jpg")');
-
-  //}
-
-
-
 function container(d) {
-
   let divArray = [];
-  let num = 100;
-
+  let num = 36;
   let imgs = [
     //'url("")',
     'url("https://live.staticflickr.com/65535/52478345852_375cf74676_b.jpg")',
@@ -42,23 +17,15 @@ function container(d) {
     'url("https://live.staticflickr.com/65535/52479394858_694416ace9_b.jpg")',
     'url("https://live.staticflickr.com/65535/52354535014_3b254741cd.jpg")'
   ];
-
   d.setup = function () {
     d.noCanvas();
-
-    // let Flwr = d.createDiv();
-    // Flwr.class('card');
-    // Flwr.id('Flwr');
-
     for (let i = 0; i < num; i++) {
       divArray.push(new new_Div(i));
     }
   }
-
   function getImg() {
     return imgs[d.floor(d.random(0, imgs.length))];
   }
-
   class new_Div {
     constructor(divID) {
       let divDefault = d.createDiv('');
